4c1072b6a06764f235692963e7ddc0aeb3799655
kwanwoo kim <kwanwoo.kim@gmail.com>
2022-10-29 19:02:38 +0900

    로그인 커스텀

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
 from . import views
+from .forms import LoginForm
+
 
 urlpatterns = [
     path(
         "login/",
-        LoginView.as_view(template_name="accounts/login_form.html"),
+        LoginView.as_view(
+            form_class=LoginForm, template_name="accounts/login_form.html"
+        ),
         name="login",
     ),
     path("profile/", views.profile, name="profile"),
