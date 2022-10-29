30d1936b78a6881bae53e2139dc52b17ffa63d90
kwanwoo kim <kwanwoo.kim@gmail.com>
2022-10-29 09:34:41 +0900

    사용자 프로필 페이지 및 프로필 수정 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path(
@@ -7,4 +8,6 @@ urlpatterns = [
         LoginView.as_view(template_name="accounts/login_form.html"),
         name="login",
     ),
+    path("profile/", views.profile, name="profile"),
+    path("profile/edit/", views.profile_edit, name="profile_edit"),
 ]
